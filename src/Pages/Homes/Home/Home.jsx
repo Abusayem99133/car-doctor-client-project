691dcec8b586871938a7a77267b540e187bdaf50
Md.Rokonuzzaman Sayem <sayem99133@gmail.com>
2024-05-01 09:28:40 +0600

    full project setup done and router and navbar,banner,footer,home added to ui client site

---
+++ src/Pages/Homes/Home/Home.jsx
@@ -0,0 +1,12 @@
+import Banner from "../Banner/Banner";
+
+const Home = () => {
+  return (
+    <div>
+      <Banner></Banner>
+      <h1 className="text-4xl text-center text-red-600">This is Home</h1>
+    </div>
+  );
+};
+
+export default Home;
