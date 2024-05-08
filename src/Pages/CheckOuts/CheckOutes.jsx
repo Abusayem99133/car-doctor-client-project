8b2bed16dd6671ef937c7cefdc6831a42b0ba6e3
Md.Rokonuzzaman Sayem <sayem99133@gmail.com>
2024-05-08 20:35:02 +0600

    checkout section private and update section private

--- src/Pages/CheckOuts/CheckOutes.jsx
+++ src/Pages/CheckOuts/CheckOutes.jsx
@@ -1,27 +1,55 @@
-const CheckOutes = ({ checkOut }) => {
-  const { img, date, service, price } = checkOut;
+const CheckOutes = ({ checkOut, handleDelete, handleCheckOutConfirm }) => {
+  const { _id, img, date, service, price, status } = checkOut;
+
   return (
     <div>
-      <tr>
-        <th>
-          <label>
-            <input type="checkbox" className="checkbox" />
-          </label>
-        </th>
-        <td>
-          <div className="avatar">
-            <div className="rounded w-24 h-24">
-              {img && <img src={img} alt="Avatar Tailwind CSS Component" />}
+      <tbody>
+        <tr>
+          <th>
+            <button
+              onClick={() => handleDelete(_id)}
+              className="btn btn-circle btn-outline"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          </th>
+          <td>
+            <div className="avatar">
+              <div className="rounded w-24 h-24">
+                {img && <img src={img} alt="Avatar Tailwind CSS Component" />}
+              </div>
             </div>
-          </div>
-        </td>
-        <td>{service}</td>
-        <td>{date}</td>
-        <td>$ {price}</td>
-        <th>
-          <button className="btn btn-ghost btn-xs">details</button>
-        </th>
-      </tr>
+          </td>
+          <td>{service}</td>
+          <td>{date}</td>
+          <td>$ {price}</td>
+          <th>
+            {status === "confirm" ? (
+              <span className="font-bold text-primary">confirmed</span>
+            ) : (
+              <button
+                onClick={() => handleCheckOutConfirm(_id)}
+                className="btn btn-ghost btn-xs"
+              >
+                Confirm
+              </button>
+            )}
+          </th>
+        </tr>
+      </tbody>
     </div>
   );
 };
--- src/Pages/CheckOuts/CheckOuts.jsx
+++ src/Pages/CheckOuts/CheckOuts.jsx
@@ -13,29 +13,70 @@ const CheckOuts = () => {
         console.log(data);
         setCheckOuts(data);
       });
-  }, []);
+  }, [url]);
+  const handleDelete = (id) => {
+    const proceed = confirm("Are You sure you want to delete");
+    if (proceed) {
+      fetch(`http://localhost:5000/checkOuts/${id}`, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          if (data.deletedCount > 0) {
+            alert("deleted successful");
+            const remaining = checkouts.filter(
+              (checkout) => checkout._id !== id
+            );
+            setCheckOuts(remaining);
+          }
+        });
+    }
+  };
+  const handleCheckOutConfirm = (id) => {
+    fetch(`http://localhost:5000/checkOuts/${id}`, {
+      method: "PATCH",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ status: "confirm" }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.modifiedCount > 0) {
+          const remaining = checkouts.filter((checkOut) => checkOut._id !== id);
+          const updated = checkouts.find((checkOut) => checkOut._id === id);
+          updated.status = "confirm";
+          const newCheckOuts = [updated, ...remaining];
+          setCheckOuts(newCheckOuts);
+        }
+      });
+  };
   return (
     <div>
       <h2>Your CheckOut: {checkouts.length}</h2>
       <div className="overflow-x-auto w-full">
         <table className="table w-full">
           {/* head */}
-          <thead>
-            <tr>
-              <th>
-                <label>
-                  <input type="checkbox" className="checkbox" />
-                </label>
-              </th>
-              <th>Name</th>
-              <th>Job</th>
-              <th>Favorite Color</th>
-              <th></th>
-            </tr>
-          </thead>
+
           <tbody>
+            <thead>
+              <tr>
+                <th>Image</th>
+                <th>Service</th>
+                <th>Date</th>
+                <th>Price</th>
+                <th>Status</th>
+              </tr>
+            </thead>
             {checkouts?.map((checkout) => (
-              <CheckOutes key={checkout._id} checkOut={checkout}></CheckOutes>
+              <CheckOutes
+                key={checkout._id}
+                checkOut={checkout}
+                handleDelete={handleDelete}
+                handleCheckOutConfirm={handleCheckOutConfirm}
+              ></CheckOutes>
             ))}
           </tbody>
         </table>
